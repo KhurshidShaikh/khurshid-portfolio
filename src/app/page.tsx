c882f11a2df6a7ed97805f2b2c374e59e9b921c3
khurshid <khurshidsk7304@gmail.com>
2025-08-08 22:47:05 +0530

    resume updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -95,7 +95,7 @@ export default function Home() {
                   Let&apos;s Connect
                 </a>
                 <a
-                  href="https://drive.google.com/file/d/1VXv_HCxaZNW9Q-IV867PN6roybBzs1Wv/view?usp=sharing"
+                  href="https://drive.google.com/file/d/11oKdix4hFOnbbknd0PNuR40jXYmbNHuK/view?usp=sharing"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="inline-block bg-white dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-700 text-blue-500 border border-blue-500 font-semibold text-sm sm:text-sm md:text-base px-5 sm:px-6 md:px-8 py-3 sm:py-3 rounded-full transition-colors duration-300 whitespace-nowrap"
